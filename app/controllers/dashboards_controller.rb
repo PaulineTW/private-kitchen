9b861436a8a980ce2445aebaf3e56f0e610307e4
pauline rouger <paulinerouge@hotmail.com>
2021-12-03 17:06:32 +0800

    Added dashboardcontroller + routes + views

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,18 @@
 class DashboardsController < ApplicationController
+
+  # def users
+  #    @users = User.all
+  #    respond_to do |format|
+  #     format.html {render :layout => 'dashboard'}
+  #   end
+  # end
+
+  def my_bookings
+    @bookings = Booking.all
+  end
+
+  def my_kitchens
+    @kitchens = Kitchen.all
+  end
+
 end
