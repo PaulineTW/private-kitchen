01ac63aceffee6885c24e1dfde685640a6a545db
Julianne Tran <julianne.tran03@gmail.com>
2021-12-04 15:02:48 +1100

    add kitchen relationship to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 has_many :bookings, dependent: :destroy
+has_many :kitchens, dependent: :destroy
 end
