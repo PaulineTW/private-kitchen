403233b8a427decdc625cac042584e0dfd1a66e0
pauline rouger <paulinerouge@hotmail.com>
2021-12-04 10:46:04 +0800

    Displayed all bookings in Dashboard from customers pespective

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def dashboards
+    @bookings = Booking.all
+    @kitchens = Kitchen.all
+  end
 end
