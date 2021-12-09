07ddb29bb60a240743f1979f4dadc9a6f2eed96c
elliott fuller <efuller1101@gmail.com>
2021-12-09 19:13:43 +1100

    delete

--- db/migrate/20211209075317_remove_users_from_bookings.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUsersFromBookings < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :bookings, :users_id, :user_id
-    rename_column :bookings, :kitchens_id, :kitchen_id
-  end
-end
